87d670d611496a4e46353db07a383d39ee665307
Paulo Gomes da Cruz Junior <paulushc@gmail.com>
2021-08-18 17:41:30 -0700

    Primeiro Commit

---
+++ src/main/java/io/github/paulushcgcj/paulushcgcjv0/PaulushcgcjV0Application.java
@@ -0,0 +1,13 @@
+package io.github.paulushcgcj.paulushcgcjv0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaulushcgcjV0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaulushcgcjV0Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/paulushcgcj/paulushcgcjv0/PaulushcgcjV0ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.paulushcgcj.paulushcgcjv0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaulushcgcjV0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
